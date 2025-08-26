955b8b425e6184b81b71e9a7388ee4948c65257b
Kytsu01 <nicolasmfrancisco@gmail.com>
2025-08-26 15:57:36 -0300

    fix: rename Post columns from Name/Description to Title/Body

---
+++ JsonPlaceholderImporter/Migrations/20250826184737_RenameColumnsInPosts.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JsonPlaceholderImporter.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameColumnsInPosts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Posts",
+                newName: "Title");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Posts",
+                newName: "Body");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Posts",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "Body",
+                table: "Posts",
+                newName: "Description");
+        }
+    }
+}
--- JsonPlaceholderImporter/Models/Post.cs
+++ JsonPlaceholderImporter/Models/Post.cs
@@ -5,8 +5,8 @@ namespace JsonPlaceholderImporter.Models
     public class Post
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
 
         public int UserId { get; set; }
         public User? User { get; set; }
