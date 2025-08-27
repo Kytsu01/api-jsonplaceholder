7bcbc53546392a4ce70a077483a317b849df07b3
Kytsu01 <nicolasmfrancisco@gmail.com>
2025-08-26 23:41:20 -0300

    fix: rename Todo column IsFinished to Completed

---
+++ JsonPlaceholderImporter/Migrations/20250827024029_RenameTodoIsFinishedToCompleted.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JsonPlaceholderImporter.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTodoIsFinishedToCompleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsFinished",
+                table: "Todos",
+                newName: "Completed");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Completed",
+                table: "Todos",
+                newName: "IsFinished");
+        }
+    }
+}
--- JsonPlaceholderImporter/Models/Todo.cs
+++ JsonPlaceholderImporter/Models/Todo.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
-        public bool IsFinished { get; set; }
+        public bool Completed { get; set; }
 
         public int UserId { get; set; }
         public User? User { get; set; }
